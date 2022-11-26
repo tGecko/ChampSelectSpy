e2fb90139114dfb503e72c6e6fafa50e8fa68282
thoma <thoma@DESKTOP-R505QMC>
2022-11-26 14:40:36 +0100

    Projektdateien hinzufügen.

---
+++ ChampSelectNames/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace ChampSelectNames
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmr1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnOpGGAll = new System.Windows.Forms.Button();
+            this.chkAutoOPGG = new System.Windows.Forms.CheckBox();
+            this.cmbRegion = new System.Windows.Forms.ComboBox();
+            this.txtParticipants = new System.Windows.Forms.TextBox();
+            this.btnUGGALL = new System.Windows.Forms.Button();
+            this.chkAutoUGG = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tmr1
+            // 
+            this.tmr1.Enabled = true;
+            this.tmr1.Interval = 500;
+            this.tmr1.Tick += new System.EventHandler(this.tmr1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 158);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(326, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // btnOpGGAll
+            // 
+            this.btnOpGGAll.Location = new System.Drawing.Point(12, 98);
+            this.btnOpGGAll.Name = "btnOpGGAll";
+            this.btnOpGGAll.Size = new System.Drawing.Size(75, 23);
+            this.btnOpGGAll.TabIndex = 10;
+            this.btnOpGGAll.Text = "OP.GG All";
+            this.btnOpGGAll.UseVisualStyleBackColor = true;
+            this.btnOpGGAll.Click += new System.EventHandler(this.btnOpGGAll_Click);
+            // 
+            // chkAutoOPGG
+            // 
+            this.chkAutoOPGG.AutoSize = true;
+            this.chkAutoOPGG.Location = new System.Drawing.Point(93, 102);
+            this.chkAutoOPGG.Name = "chkAutoOPGG";
+            this.chkAutoOPGG.Size = new System.Drawing.Size(112, 17);
+            this.chkAutoOPGG.TabIndex = 11;
+            this.chkAutoOPGG.Text = "Auto open OP.GG";
+            this.chkAutoOPGG.UseVisualStyleBackColor = true;
+            // 
+            // cmbRegion
+            // 
+            this.cmbRegion.FormattingEnabled = true;
+            this.cmbRegion.Items.AddRange(new object[] {
+            "EUW",
+            "NA",
+            "EUNE",
+            "OCE",
+            "KR",
+            "JP",
+            "BR",
+            "LAS",
+            "LAN",
+            "RU"});
+            this.cmbRegion.Location = new System.Drawing.Point(247, 100);
+            this.cmbRegion.Name = "cmbRegion";
+            this.cmbRegion.Size = new System.Drawing.Size(67, 21);
+            this.cmbRegion.TabIndex = 12;
+            // 
+            // txtParticipants
+            // 
+            this.txtParticipants.Location = new System.Drawing.Point(12, 3);
+            this.txtParticipants.Multiline = true;
+            this.txtParticipants.Name = "txtParticipants";
+            this.txtParticipants.ReadOnly = true;
+            this.txtParticipants.Size = new System.Drawing.Size(302, 89);
+            this.txtParticipants.TabIndex = 13;
+            // 
+            // btnUGGALL
+            // 
+            this.btnUGGALL.Location = new System.Drawing.Point(12, 127);
+            this.btnUGGALL.Name = "btnUGGALL";
+            this.btnUGGALL.Size = new System.Drawing.Size(75, 23);
+            this.btnUGGALL.TabIndex = 14;
+            this.btnUGGALL.Text = "U.GG All";
+            this.btnUGGALL.UseVisualStyleBackColor = true;
+            this.btnUGGALL.Click += new System.EventHandler(this.btnUGGALL_Click);
+            // 
+            // chkAutoUGG
+            // 
+            this.chkAutoUGG.AutoSize = true;
+            this.chkAutoUGG.Location = new System.Drawing.Point(93, 131);
+            this.chkAutoUGG.Name = "chkAutoUGG";
+            this.chkAutoUGG.Size = new System.Drawing.Size(105, 17);
+            this.chkAutoUGG.TabIndex = 15;
+            this.chkAutoUGG.Text = "Auto open U.GG";
+            this.chkAutoUGG.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 180);
+            this.Controls.Add(this.chkAutoUGG);
+            this.Controls.Add(this.btnUGGALL);
+            this.Controls.Add(this.txtParticipants);
+            this.Controls.Add(this.cmbRegion);
+            this.Controls.Add(this.chkAutoOPGG);
+            this.Controls.Add(this.btnOpGGAll);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "ChampSelectSpy - 1.1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer tmr1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button btnOpGGAll;
+        private System.Windows.Forms.CheckBox chkAutoOPGG;
+        private System.Windows.Forms.ComboBox cmbRegion;
+        private System.Windows.Forms.TextBox txtParticipants;
+        private System.Windows.Forms.Button btnUGGALL;
+        private System.Windows.Forms.CheckBox chkAutoUGG;
+    }
+}
+
---
+++ ChampSelectNames/Form1.cs
@@ -0,0 +1,212 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.Security;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChampSelectNames
+{
+
+    public partial class Form1 : Form
+    {
+        private struct ClientData
+        {
+            public bool isRunning;
+            public int ProcessId;
+            public string cmdline;
+            public int RiotPort;
+            public string RiotToken;
+            public int ClientPort;
+            public string ClientToken;
+            public string Region;
+            public string SummonerDisplayName;
+            public String GameState;
+        }
+        List<string> participants = new List<string>();
+        class summoner
+        {
+            public String DisplayName;
+        }
+        private ClientData ClientInfo;
+        ManagementObject leagueProcess;
+        int leagueID;
+        ManagementClass mngmtClass;
+        String cmdline;
+        String RiotPort;
+        String RiotToken;
+        String ClientPort;
+        String ClientToken;
+        
+        Boolean LeagueRunning = false;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mngmtClass = new ManagementClass("Win32_Process");
+            cmbRegion.Text = "EUW";
+            toolStripStatusLabel1.Text = "Waiting for League...";
+            chkAutoOPGG.Checked = true;
+        }
+
+
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                int Start, End;
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+
+            return "";
+        }
+
+        private void tmr1_Tick(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName("LeagueClientUx");
+            if (processes.Length > 0)
+            {
+                ClientInfo.isRunning = true;
+                ClientInfo.ProcessId = processes[0].Id;
+                toolStripStatusLabel1.Text = "Summoner: " + ClientInfo.SummonerDisplayName + " - Gamestate: " + ClientInfo.GameState;
+                if (ClientInfo.cmdline == null)
+                {
+                    foreach (ManagementObject obj in mngmtClass.GetInstances())
+                    {
+                        if (obj["Name"].Equals("LeagueClientUx.exe"))
+                        {
+                            ClientInfo.cmdline = obj["Name"] + " [" + obj["CommandLine"] + "]";
+                            ClientInfo.RiotPort = int.Parse(getBetween(ClientInfo.cmdline, "--riotclient-app-port=", "\" \"--no-rads"));
+                            ClientInfo.RiotToken = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes("riot:" + getBetween(ClientInfo.cmdline, "--riotclient-auth-token=", "\" \"--riotclient")));
+                            ClientInfo.ClientPort = int.Parse(getBetween(ClientInfo.cmdline, "--app-port=", "\" \"--install"));
+                            ClientInfo.ClientToken = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes("riot:" + getBetween(ClientInfo.cmdline, "--remoting-auth-token=", "\" \"--respawn-command=LeagueClient.exe")));
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ClientInfo = default(ClientData);
+                toolStripStatusLabel1.Text = "Waiting for League...";
+            }
+
+            if (ClientInfo.isRunning)
+            {
+                if(ClientInfo.Region == null|| ClientInfo.Region == "")
+                {
+                    ClientInfo.Region = getBetween(MakeRequest(ClientInfo, "GET", "/lol-rso-auth/v1/authorization", client: true), "currentPlatformId\":\"", "\",\"subject").ToLower();
+                }
+
+                if(ClientInfo.SummonerDisplayName == null)
+                {
+                    summoner summ= JsonConvert.DeserializeObject<summoner>(MakeRequest(ClientInfo, "GET", "/lol-summoner/v1/current-summoner", client: true));
+                    if(summ != null)
+                    {
+                        ClientInfo.SummonerDisplayName = summ.DisplayName;
+
+                    }
+                }
+
+                ClientInfo.GameState = MakeRequest(ClientInfo, "GET", "/lol-gameflow/v1/gameflow-phase", true).Trim('"');
+                if(ClientInfo.GameState == "ChampSelect" || ClientInfo.GameState == "InProgress")
+                {
+                    if(participants.Count < 5)
+                    {
+                        DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(MakeRequest(ClientInfo, "GET", "/chat/v5/participants/champ-select", client: false));
+                        DataTable dataTable = dataSet.Tables["participants"];
+                        if (dataTable.Rows.Count == 5)
+                        {
+                            foreach (DataRow row in dataTable.Rows)
+                            {
+                                participants.Add(row["game_name"].ToString());
+                            }
+                            if(chkAutoOPGG.Checked)
+                            {
+                                btnOpGGAll_Click(null,null);
+                            }
+                            if (chkAutoUGG.Checked)
+                            {
+                                btnUGGALL_Click(null, null);
+                            }
+                        }
+                    }
+                    
+ 
+                }
+                else
+                {
+                    participants.Clear();
+                }
+                if(participants.Count > 0)
+                {
+                    txtParticipants.Text = string.Join("\r\n", participants);
+                }
+                else
+                {
+                    txtParticipants.Text = string.Empty;
+                }
+            }
+
+        }
+        private static string MakeRequest(ClientData info, string method, string url, bool client)
+        {
+            int port = client ? info.ClientPort : info.RiotPort;
+            string authToken = client ? info.ClientToken : info.RiotToken;
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+                HttpWebRequest obj = (HttpWebRequest)WebRequest.Create("https://127.0.0.1:" + port + url);
+                obj.PreAuthenticate = true;
+                obj.ContentType = "application/json";
+                obj.Method = method;
+                obj.Headers.Add("Authorization", "Basic " + authToken);
+                using StreamReader streamReader = new StreamReader(((HttpWebResponse)obj.GetResponse()).GetResponseStream());
+                return streamReader.ReadToEnd();
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        private void btnOpGGAll_Click(object sender, EventArgs e)
+        {
+            if(participants.Count > 0)
+            {
+                System.Diagnostics.Process.Start(new ProcessStartInfo
+                {
+                    FileName = "https://www.op.gg/multisearch/" + cmbRegion.Text + "?summoners=" + string.Join(",",participants),
+                    UseShellExecute = true
+                });
+            }
+        }
+
+        private void btnUGGALL_Click(object sender, EventArgs e)
+        {
+            if (participants.Count > 0)
+            {
+                System.Diagnostics.Process.Start(new ProcessStartInfo
+                {
+                    FileName = "https://u.gg/multisearch?summoners=" + string.Join(",", participants) + "&region=" + ClientInfo.Region,
+                    UseShellExecute = true
+                });
+            }
+        }
+    }
+}
